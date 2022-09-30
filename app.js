cda0a170c0008e1000261a529bc2ca9b7b516560
Hammad <ch.hammad2914@gmail.com>
2022-09-30 19:21:56 +0500

    java3a

---
+++ app.js
@@ -0,0 +1,17 @@
+function circle(){
+  var circ =  document.getElementById("shape")
+  circ.style.borderRadius= "50%";
+  circ.style.width = "300px";
+}
+
+function rectangle(){
+    var rect = document.getElementById("shape")
+    rect.style.width ="600px";
+    rect.style.borderRadius ="0";
+}
+
+function square(){
+    var sqr = document.getElementById("shape")
+    sqr.style.width ="300px";
+    sqr.style.borderRadius ="0";
+}
